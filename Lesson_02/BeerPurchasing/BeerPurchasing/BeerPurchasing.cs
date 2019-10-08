5c464f5b8563823bb189ca9550f3786ef86bdcbd
sonnov <sonnov2451@gmail.com>
2019-10-08 15:55:35 +0300

    Lesson_02(01)

---
+++ Lesson_02/BeerPurchasing/BeerPurchasing/BeerPurchasing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class BeerPurchasing
+{
+    static void Main()
+    {
+        decimal money = 56.00m;
+        decimal Obolon = 7.00m;
+        decimal Chernihivske = 20.00m;
+        decimal Stella = 30.00m;
+        decimal Bud = 45.00m;
+        decimal Leffe = 60.00m;
+        string beer = "Obolon";
+        decimal cost = Obolon;
+
+        Console.WriteLine("You have " + money);
+
+        if (money >= 60.00m)
+        {
+            beer = "Leffe";
+            cost = Leffe;
+        }
+        else if (money >= 45.00m)
+        {
+            beer = "Bud";
+            cost = Bud;
+        }
+        else if (money >= 30.00m)
+        {
+            beer = "Stella";
+            cost = Stella;
+        }
+        else if (money >= 20.00m)
+        {
+            beer = "Bud";
+            cost = Bud;
+        }
+
+
+        if (money >= 7.00m)
+        {
+            Console.WriteLine("You can buy " + beer);
+            Console.WriteLine("Your rest will be : {0}", (money - cost));
+        }
+        else
+            Console.WriteLine("You can not buy any beer");
+
+        Console.ReadKey();
+    }
+}
---
+++ Lesson_02/Discounts/Discounts/CodeFile1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Discouts
+{
+    static void Main()
+    {
+        decimal sum = 696.00m;
+        decimal discount = 0.00m;
+
+        if (sum >= 520.00m)
+        {
+            discount = 7.00m;
+        }
+        else if (sum >= 400.00m)
+        {
+            discount = 5.00m;
+        }
+        else if (sum >= 300.00m)
+        {
+            discount = 3.00m;
+        }
+
+        Console.WriteLine("Your discount is : {0}. Total to pay : {1}", discount, (sum - (sum / 100) * discount));
+
+            Console.ReadKey();
+    }
+}
