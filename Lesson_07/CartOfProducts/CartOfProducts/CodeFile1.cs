c356d97d48bdbcdd6295d4d605d2df48a985eb06
sonnov <sonnov2451@gmail.com>
2019-10-08 16:06:10 +0300

    Lesson_07(01)

---
+++ Lesson_07/CartOfProducts/CartOfProducts/CodeFile1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+class Product
+{
+    private string name;
+    private int price;
+
+    public Product(string name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public int getPrice()
+    {
+        return price;
+    }
+}
+
+
+class Cart
+{
+    Product[] products = new Product[10];
+    public void addProduct(Product product)
+    {
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (products[i] == null)
+            {
+                products[i] = product;
+                break;
+            }
+        }
+    }
+
+    public void removeProduct(string name)
+    {
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (name == products[i].getName())
+            {
+                products[i] = null;
+                break;
+            }
+        }
+    }
+
+    public void showCart()
+    {
+        Console.WriteLine("CART:");
+        int total = 0;
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (products[i] != null)
+            {
+                Console.WriteLine("\t" + products[i].getName() + " : " + products[i].getPrice());
+                total += products[i].getPrice();
+            }
+        }
+        Console.WriteLine("\t\tCART TOTAL: " + total);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product p1 = new Product("Coca-Cola", 15);
+        Product p2 = new Product("Pepsi-Cola", 17);
+        Product p3 = new Product("Milk", 26);
+        Product p4 = new Product("Jack Daniels Fire", 450);
+        Product p5 = new Product("Honey", 250);
+        Cart cart = new Cart();
+        cart.addProduct(p1);
+        cart.addProduct(p3);
+        cart.addProduct(p4);
+        cart.addProduct(p5);
+        cart.showCart();
+        cart.removeProduct("Milk");
+        cart.showCart();
+
+        Console.ReadKey();
+    }
+}
---
+++ Lesson_07/Shapes/Shapes/CodeFile1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+class Shapes
+{
+    static void Main()
+    {
+        int key;
+        Console.WriteLine("1 - Circle; 2 - Rectangle; 3 - Rectangle; 0 - Exit");
+        do
+        {
+            key = Convert.ToInt32(Console.ReadLine());
+
+            switch (key)
+            {
+                case 1:
+                    Console.WriteLine("Info about the circe :");
+                    Circle circle = new Circle(7);
+                    circle.getInfo(); 
+                    break;
+                case 2:
+                    Console.WriteLine("Info about the rectangle :");
+                    Rectangle rectangle = new Rectangle(8, 8);
+                    rectangle.getInfo();
+                    break;
+                case 3:
+                    Console.WriteLine("Info about the triangle :");
+                    Triangle triangle = new Triangle(10, 6, 6);
+                    triangle.getInfo();
+                    break;
+            }
+
+        } while (key != 0);
+
+        Console.ReadKey();
+    }
+}
+
+class Shape
+{
+    Random rnd = new Random();
+    private string[] colors = new string[] {"green", "gray", "pink", "black", "purple",
+        "white", "red", "yellow", "brown", "blue", };
+    private string name;
+    private int perimeter;
+    private double area;
+    private string color;
+
+    public void setColor()
+    {
+        color = colors[rnd.Next(0, 10)];
+    }
+    public string getColor()
+    {
+        return color;
+    }
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public void setPerimeter(int perimeter)
+    {
+        this.perimeter = perimeter;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public int getPerimeter()
+    {
+        return perimeter;
+    }
+    public void setArea(double area)
+    {
+        this.area = area;
+    }
+    public double getArea()
+    {
+        return area;
+    }
+    public void getInfo()
+    {
+        Console.WriteLine("Perimeter = " + getPerimeter() + "; Area = " + getArea() + "; Name = " +
+            getName() + "; Color = " + getColor());
+    }
+}
+class Circle : Shape
+{
+    private int radius;
+    private const double PI = 3.14159265358979;
+    //private double circuit;
+
+    public Circle(int radius)
+    {
+        setArea(2 * PI * Math.Pow(radius, 2));
+        setName("circle");
+        setColor();
+    }
+}
+class Triangle : Shape
+{
+    private int side1;
+    private int side2;
+    private int side3;
+
+    public Triangle(int side1, int side2, int side3)
+    {
+        setPerimeter(side1 + side2 + side3);
+        setColor();
+        setArea(Math.Sqrt(getPerimeter() / 2 * (getPerimeter() - side1) * (getPerimeter() - side2) * (getPerimeter() - side3)));
+        if (side1 == side2 & side2 == side3)
+        {
+            setName("equilateral");
+        }
+        else if (side1 == side2 & side3 != (side1 & side2) | side1 == side3 & side2 != (side1 & side3) | side2 == side3 & side1 != (side2 & side3))
+        {
+            setName("isosceles");
+        }
+        else
+        {
+            setName("versatile");
+        }
+    }
+
+}
+class Rectangle : Shape
+{
+    private int width;
+    private int lenght;
+
+    public Rectangle(int width, int lenght)
+    {
+        setPerimeter(2 * (width + lenght));
+        setArea(width * lenght);
+        setName(width == lenght ? "square" : "rectangle");
+        setColor();
+    }
+}
