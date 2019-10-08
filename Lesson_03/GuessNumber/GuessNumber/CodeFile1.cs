936e8f8febbb6be960a05cb86d1cc7fedd7a765f
sonnov <sonnov2451@gmail.com>
2019-10-08 15:59:32 +0300

    Lesson_03(01)

---
+++ Lesson_03/GuessNumber/GuessNumber/CodeFile1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class GuessNumber
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(0, 100);
+        int input;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Try number {0}. Enter your number : ", i);
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input > value)
+            {
+                Console.WriteLine("Wrong! My number is lesser.");
+            }
+            else if (input < value)
+            {
+                Console.WriteLine("Wrong! My number is bigger.");
+            }
+            else
+            {
+                Console.WriteLine("Exactly! You win!");
+                break;
+            }
+            if (i == 5 & input != value)
+            {
+                Console.WriteLine("Lose!");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Lesson_03/NumbersCount/NumbersCount/CodeFile1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class NumbersCount
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number :");
+        int value;
+        while (true)
+        {
+            value = Convert.ToInt32(Console.ReadLine());
+            if (value >= 0)
+            {
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        int sum = 0;
+        int i, j;
+        i = j = 0;
+
+        while (value > 0)
+        {
+            if ((value % 10)% 2 == 0)
+            {
+                sum += value % 10;
+            }
+
+            if ((value % 10) % 3 == 0)
+            {
+                j++;
+            }
+
+            value /= 10;
+            i++;
+        }
+
+        Console.WriteLine("Amount of numbers is : {0}", i);
+        Console.WriteLine("Sum of pair numbers is : {0}", sum);
+        Console.WriteLine("Amount of numbers divided completely by 3 is : {0}", j);
+
+        Console.ReadKey();
+    }
+}
